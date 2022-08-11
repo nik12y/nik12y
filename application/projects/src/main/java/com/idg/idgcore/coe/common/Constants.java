509c302be32bd9c7e7cd533af79c75c5c9dba91e
Chhaital Maagade <102149842+chhaitalmaagade@users.noreply.github.com>
2022-08-11 20:53:35 +0530

    Local commit done at 8:53pm on 11August2022

--- application/projects/src/main/java/com/idg/idgcore/coe/common/Constants.java
+++ application/projects/src/main/java/com/idg/idgcore/coe/common/Constants.java
@@ -73,7 +73,9 @@ public final class Constants {
 
     public static final String IBAN = "IBAN";
     public static final String IBAN_SERVICE = "ibanApplicationService";
-    public static final String RISKCATEGORY = "RISKCATEGORY";                                                                                    public static final String RISKCATEGORY_SERVICE = "riskCategoryApplicationService";                                       public static final String RISKCODE = "RISKCODE";
+
+    public static final String RISKCATEGORY_SERVICE = "riskCategoryApplicationService";
+    public static final String RISKCODE = "RISKCODE";
     public static final String RISKCODE_SERVICE = "riskCodeApplicationService";
     public static final String PURGING_POLICY = "PURGING_POLICY";
     public static final String PURGING_POLICY_SERVICE = "purgingApplicationService";
@@ -89,7 +91,7 @@ public final class Constants {
 
 
 
-    public static final String RISKCATEGORY_SERVICE = "riskCategoryApplicationService";
+
     private Constants () {
     }
 
