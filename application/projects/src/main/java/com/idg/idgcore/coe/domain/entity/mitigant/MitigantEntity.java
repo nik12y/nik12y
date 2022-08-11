fe08c8785f40c29d24305670181e7408c3cefd02
Aditya.G <aditya.g@idigicloudtech.com>
2022-08-11 11:29:16 +0530

    mitigant code commit 11-08-2022

--- application/projects/src/main/java/com/idg/idgcore/coe/domain/entity/mitigant/MitigantEntity.java
+++ application/projects/src/main/java/com/idg/idgcore/coe/domain/entity/mitigant/MitigantEntity.java
@@ -1,11 +1,25 @@
 package com.idg.idgcore.coe.domain.entity.mitigant;
 
 import com.idg.idgcore.domain.AbstractAuditableDomainEntity;
-import lombok.*;
-
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import javax.persistence.Entity;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import javax.persistence.Inheritance;
+import javax.persistence.IdClass;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import java.io.Serializable;
 import java.util.List;
+
 
 @Setter
 @Getter
@@ -19,19 +33,33 @@ import java.util.List;
 public class MitigantEntity extends AbstractAuditableDomainEntity implements Serializable {
 
     @Id
+    @Column(name = "mitigant_code")
     private String mitigantCode;
+    @Column(name = "mitigant_code_name")
     private String mitigantCodeName;
+    @Column(name = "mitigant_code_desc")
     private String mitigantCodeDesc;
+    @Column(name = "is_allow_modification")
     private char isAllowModification;
+    @Column(name = "is_actionable")
     private char isActionable;
+
+    @Column(name = "life_cycle_id")
+    private String lifeCycleId;
+    @Column(name = "reference_no")
+    private String referenceNo;
 
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "mitigant_code")
     private List<MitigantRiskCodeEntity> mitigantRiskCode;
 
+    @Column(name = "record_status")
     private String status;
+    @Column(name = "record_version")
     private Integer recordVersion;
+    @Column(name = "is_authorized")
     private String authorized;
+    @Column(name = "last_configuration_action")
     private String lastConfigurationAction;
 
 }
--- application/projects/src/main/java/com/idg/idgcore/coe/domain/entity/mitigant/MitigantRiskCodeEntity.java
+++ application/projects/src/main/java/com/idg/idgcore/coe/domain/entity/mitigant/MitigantRiskCodeEntity.java
@@ -1,9 +1,19 @@
 package com.idg.idgcore.coe.domain.entity.mitigant;
 
 import com.idg.idgcore.domain.AbstractAuditableDomainEntity;
-import lombok.*;
-
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import javax.persistence.Entity;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import javax.persistence.Inheritance;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.io.Serializable;
 
 @Setter
@@ -20,7 +30,9 @@ public class MitigantRiskCodeEntity extends AbstractAuditableDomainEntity implem
     @Column(name = "mitigant_risk_code_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer mitigantRiskCodeId;
+    @Column(name = "risk_code")
     private String riskCode;
+    @Column(name = "risk_name")
     private String riskName;
 
 }
