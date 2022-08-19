728f9c86a3f305d8b63d4e923f699dd721129301
Nilesh Bandal <nilesh.b@idigicloudtech.com>
2022-08-19 18:47:27 +0530

    [Change] Column name changes for audit, muations, country entity.

--- application/projects/src/main/java/com/idg/idgcore/coe/domain/entity/country/CountryEntity.java
+++ application/projects/src/main/java/com/idg/idgcore/coe/domain/entity/country/CountryEntity.java
@@ -7,12 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Setter
@@ -26,21 +21,40 @@ import java.io.Serializable;
 @IdClass (CountryEntityKey.class)
 public class CountryEntity extends AbstractAuditableDomainEntity implements Serializable {
     @Id
+    @Column (name="country_code")
     private String countryCode;
+    @Column (name="country_name")
     private String countryName;
+    @Column (name="country_code_machine")
     private String countryCodeMachine;
+    @Column (name="country_code_alternate")
     private String countryCodeAlternate;
+    @Column (name="region_code")
     private String regionCode;
+    @Column (name="currency_limit")
     private String currencyLimit;
+    @Column (name="overall_limit")
     private Float overallLimit;
+    @Column (name="is_iban")
     private char isIban;
+    @Column (name="is_eu_member")
     private char isEuMember;
+    @Column (name="is_bic_code")
     private char isBicCode;
+    @Column (name="is_mt_generate")
     private char isMtGenerate;
+    @Column (name="is_clearing_network")
     private char isClearingNetwork;
+    @Column (name="record_status")
     private String status;
+    @Column (name="record_version")
     private Integer recordVersion;
+    @Column (name="is_authorized")
     private String authorized;
+    @Column (name="last_configuration_action")
     private String lastConfigurationAction;
-
+    @Column (name="life_cycle_id")
+    private String lifeCycleId;
+    @Column (name="reference_no")
+    private String referenceNo;
 }
