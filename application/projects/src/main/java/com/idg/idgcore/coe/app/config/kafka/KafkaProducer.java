a7a959eb34d802014f4273e2a7c1c1a4b7d3e4ea
Nilesh Bandal <nilesh.b@idigicloudtech.com>
2022-08-22 16:18:19 +0530

    [middleware-coreengine] Kafka changes for retry,autocommit,timeout.

--- application/projects/src/main/java/com/idg/idgcore/coe/app/config/kafka/KafkaProducer.java
+++ application/projects/src/main/java/com/idg/idgcore/coe/app/config/kafka/KafkaProducer.java
@@ -1,19 +1,19 @@
 package com.idg.idgcore.coe.app.config.kafka;
 
-import com.idg.idgcore.coe.exception.*;
-import com.idg.idgcore.coe.exception.Error;
-import org.springframework.beans.factory.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import com.idg.idgcore.coe.exception.Error;
+import com.idg.idgcore.coe.exception.ExceptionUtil;
 
 @Component
 public class KafkaProducer {
     @Autowired
     KafkaTemplate<String, String> kafkaTemplate;
 
-    public String sendMessage(final String topic, final String key, final String message)
-    {
+    public String sendMessage (final String topic, final String key, final String message) {
         try {
+            kafkaTemplate.getProducerFactory().reset();
             kafkaTemplate.send(topic, key, message);
         }
         catch (Exception e) {
@@ -21,4 +21,5 @@ public class KafkaProducer {
         }
         return "Message published successfully";
     }
+
 }
